b7f00d21133ac43ecc9860fbf8332ff3fc639d28
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-09 19:55:48 +0200

    squash!

--- node-express-typescript/tests/out/test.js
+++ node-express-typescript/tests/out/test.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var assert = require("assert");
 //var assert = require("assert");
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
-        });
-    });
+describe("Array", function () {
+	describe("#indexOf()", function () {
+		it("should return -1 when the value is not present", function () {
+			assert.equal(-1, [1, 2, 3].indexOf(5));
+			assert.equal(-1, [1, 2, 3].indexOf(0));
+		});
+	});
 });
 //# sourceMappingURL=test.js.map
